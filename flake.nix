de80258e6f47411101da70f684024a31a64e10ad
Carsten Csiky <mail@carstencsiky.de>
2025-04-27 21:14:32 +0200

    flake

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Development shell with Playwright and Python";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    playwright.url = "github:pietdevries94/playwright-web-flake";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, playwright }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (final: prev: {
+              # inherit (playwright.packages.${system}) playwright-test playwright-driver;
+            })
+          ];
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            (pkgs.python3.withPackages (ps: [
+              ps.tomli
+              ps.playwright
+            ]))
+            # pkgs.python3
+            pkgs.certbot
+            pkgs.python3Packages.pip
+            # pkgs.python3Packages.playwright
+            # pkgs.python3Packages.tomli
+            pkgs.playwright-test
+          ];
+
+          shellHook = ''
+            export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
+            export PLAYWRIGHT_BROWSERS_PATH="${pkgs.playwright-driver.browsers}"
+            echo "Welcome to the Playwright and Python development shell!"
+          '';
+        };
+      });
+}
